9a7eaac104b35751e1cb9863b4cde76aee074f5d
Kranti <darkphoton20@gmail.com>
2026-07-21 21:20:33 +0800

    feat(wallet): error handling map sloggin properly

--- game-server/wallet-service/internal/account/grpc/handler.go
+++ game-server/wallet-service/internal/account/grpc/handler.go
@@ -16,8 +16,6 @@ import (
 )
 
 // INBOUND Adapter
-// Satisfies the port of the usecase to pipe external grpc calls into the
-// usecase.
 
 type Handler struct {
 	// grpc
@@ -34,9 +32,10 @@ type AccountReader interface {
 	Execute(ctx context.Context, memberID uuid.UUID) (*dto.AccountDetails, error)
 }
 
-func NewHandler(createAccountUC *usecase.CreateAccountUC) *Handler {
+func NewHandler(createAccountUC *usecase.CreateAccountUC, accountReader AccountReader) *Handler {
 	return &Handler{
 		createAccountUC: createAccountUC,
+		accountReader:   accountReader,
 	}
 }
 
@@ -60,29 +59,45 @@ func (h *Handler) GetAccount(ctx context.Context, req *pb.GetAccountRequest) (*p
 }
 
 func mapError(ctx context.Context, err error) error {
+	var code codes.Code
+	var msg string
+	logLevel := slog.LevelWarn
+
 	switch {
-	case errors.Is(err, account.ErrConcurrentModification):
+	// withRetry helper returns ErrMaxRetries, ErrConcurrentModification is internal
+	// but leaving it here for defense
+	case errors.Is(err, usecase.ErrMaxRetries) || errors.Is(err, account.ErrConcurrentModification):
 		// OCC version mismatch. caller can retry with fresh state.
-		return status.Error(codes.Aborted, "aborted")
+		code = codes.Aborted
+		msg = "aborted"
 	case errors.Is(err, commonconstants.ErrDuplicateResource):
 		return status.Error(codes.AlreadyExists, "already exists")
 	case errors.Is(err, commonconstants.ErrNotFound):
 		return status.Error(codes.NotFound, "not found")
-	// NOTE: retry worthy
+	// NOTE: retry worthy error
+	// log level warn, worth noting rate
 	case errors.Is(err, commonconstants.ErrTransient):
 		return status.Error(codes.Unavailable, "unavailable")
 	// request structurally valid, but account state doesnt allow, or violates the
-	// system constraints like FK , null when supposed to be NOT NULL, etc
+	// system constraints like FK, null when supposed to be NOT NULL, etc
 	case errors.Is(err, commonconstants.ErrConstraintViolation) || errors.Is(err, account.ErrHoldsExceedBalanace):
-		return status.Error(codes.FailedPrecondition, "failed precondition")
+		msg = "failed precondition"
+		code = codes.FailedPrecondition
+
+		// expected error, normal operations, but for tracking where things went wrong
+		// if a bug is reported and we need to trace it
+		logLevel = slog.LevelInfo
 
 	case errors.Is(err, account.ErrInvalidAmount) || errors.Is(err, account.ErrInvalidGold):
 		return status.Error(codes.InvalidArgument, "invalid argument")
 
 	default:
-		// unexpected error
-		slog.ErrorContext(ctx, "unexpected error")
-		return status.Error(codes.Internal, "internal")
+		// unexpected, unhandled error
+		code = codes.Internal
+		msg = "unhandled error"
+		logLevel = slog.LevelError
 	}
 
+	slog.Log(ctx, logLevel, msg, "code", code)
+	return status.Error(code, msg)
 }
