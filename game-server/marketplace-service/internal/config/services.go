2ecc532fbe581d53288258740a82ee1807df6b6d
Nick Peng <gibson7780@gmail.com>
2026-07-25 20:23:21 +0800

    marketplace basic structure

---
+++ game-server/marketplace-service/internal/config/services.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"context"
+
+	accountgrpc "github.com/darkphotonKN/barrowspire-server/wallet-service/internal/account/grpc"
+	accountquery "github.com/darkphotonKN/barrowspire-server/wallet-service/internal/account/query"
+	accountrepo "github.com/darkphotonKN/barrowspire-server/wallet-service/internal/account/repository"
+	"github.com/darkphotonKN/barrowspire-server/wallet-service/internal/account/usecase"
+	"github.com/jmoiron/sqlx"
+)
+
+// sets up all services and their dependency injections at
+// server start once.
+
+type Services struct {
+	AccHandler *accountgrpc.Handler
+}
+
+func NewServices(ctx context.Context, db *sqlx.DB) *Services {
+	accountRepo := accountrepo.NewAccountRepository(db)
+	placeHoldUC := usecase.NewPlaceHoldUC(accountRepo)
+	createAccUC := usecase.NewCreateAccountUC(accountRepo)
+	getAccQuery := accountquery.NewGetAccountQuery(db)
+	accHandler := accountgrpc.NewHandler(createAccUC, placeHoldUC, getAccQuery)
+
+	return &Services{
+		AccHandler: accHandler,
+	}
+}
